377de6c37aec9b35d83f5e29f57d3fc846ebd6a6
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-09-15 16:26:36 +0900

    とりあえず箱だけ用意しました

---
+++ src/Preprocessing.py
@@ -0,0 +1,11 @@
+
+"""
+提供データを読み込み、データに前処理を施し、モデルに入力が可能な状態でファイル出力するモジュール。
+get_train_dataやget_test_dataのように、学習用と評価用を分けて、前処理を行う関数を定義してください。
+"""
+
+def get_train_data():
+    return None
+
+def get_test_data():
+    return None
