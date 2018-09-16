87765dc109bc4732d5d176c571a716d558e6ce25
Daiya-Mita <daiyamaxus@gmail.com>
2018-09-16 18:41:20 +0900

    update

--- src/U-net_with_simple_Resnet_Blocks.py
+++ src/U-net_with_simple_Resnet_Blocks.py
@@ -1,51 +1,31 @@
 
-# とりあえずこれの写経
-# https://www.kaggle.com/divrikwicky/u-net-with-simple-resnet-blocks-forked
-
 import os
-import sys
-import random
-import gc
-import time
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
+import gc
 
-# import cv2
-from sklearn.model_selection import train_test_split
-
-from tqdm import tqdm, tnrange
-from itertools import chain
-from skimage.io import imread, imshow, concatenate_images
-from skimage.transform import resize
-from skimage.morphology import label
-
-from keras.models import Model, load_model
-from keras.layers import Input,Dropout,BatchNormalization,Activation,Add
-from keras.layers.core import Lambda
+from keras.models import Model
+from keras.layers import Input, Dropout, BatchNormalization, Activation, Add
 from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras import backend as K
-
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from Utils import my_iou_metric, loadpkl, filter_image, iou_metric, iou, cov_to_class, rle_encode
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+from Utils import loadpkl, IMG_SIZE_TARGET, upsample, downsample, my_iou_metric, save2pkl, NUM_FOLDS
 from Preprocessing import get_input_data
+
+"""
+Preprocessingで作成したファイルを読み込み、モデルを学習するモジュール。
+学習済みモデルや特徴量、クロスバリデーションの評価結果を出力する関数も定義してください。
+"""
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ACTIVATION = "relu"
-iou_thresholds = np.array([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
-
-# predict both orginal and reflect x
-def predict_result(model,x_test,img_size_target):
-    preds_test = model.predict(x_test).reshape(-1, img_size_target, img_size_target)
-    preds_test += np.array([ np.fliplr(a) for a in model.predict(np.array([np.fliplr(x) for x in x_test])).reshape(-1, img_size_target, img_size_target)])
-    return preds_test / 2.0
 
 def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
     x = Conv2D(filters, size, strides=strides, padding=padding)(x)
@@ -147,22 +127,85 @@ def build_model(input_layer, start_neurons, DropoutRatio = 0.5):
 
     return output_layer
 
+# k-fold用に作っておきます
+def kfold_training(train_df, test_df, num_folds, stratified = True, debug= False):
+
+    # coverage_class以外のカラム名
+    feats = [f for f in train_df.columns if f not in ['coverage_class']]
+
+    # cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits= num_folds, shuffle=True, random_state=47)
+    else:
+        folds = KFold(n_splits= num_folds, shuffle=True, random_state=47)
+
+    X = np.array(train_df.images.tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
+    Y = np.array(train_df.masks.tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
+    cov = train_df.coverage.values
+    depth = train_df.z.values
+
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['coverage_class'])):
+
+        # Create train/validation split stratified by salt coverage
+        ids_train, ids_valid = train_df.index.values[train_idx], train_df.index.values[valid_idx]
+        x_train, y_train = X[train_idx], Y[train_idx]
+        x_valid, y_valid = X[valid_idx], Y[valid_idx]
+        cov_train, cov_test = train_df.coverage.values[train_idx], train_df.coverage.values[valid_idx]
+        depth_train, depth_test = train_df.z.values[train_idx], train_df.z.values[valid_idx]
+
+        # save validation data
+        save2pkl('../output/x_valid'+str(n_fold)+'.pkl', x_valid)
+        save2pkl('../output/y_valid'+str(n_fold)+'.pkl', y_valid)
+
+        # Data augmentation
+        x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
+        y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
+        print("train shape: {}, test shape: {}".format(x_train.shape, y_train.shape))
+
+        # model
+        input_layer = Input((IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1))
+        output_layer = build_model(input_layer, 16,0.5)
+
+        model = Model(input_layer, output_layer)
+        model.compile(loss="binary_crossentropy", optimizer="adam", metrics=[my_iou_metric])
+
+        early_stopping = EarlyStopping(monitor='val_my_iou_metric', mode = 'max', patience=20, verbose=1)
+        model_checkpoint = ModelCheckpoint('../output/unet_best'+str(n_fold)+'.model',monitor='val_my_iou_metric', mode = 'max', save_best_only=True, verbose=1)
+        reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric', mode = 'max',factor=0.2, patience=5, min_lr=0.00001, verbose=1)
+
+        epochs = 200
+        batch_size = 32
+
+        history = model.fit(x_train, y_train,
+                            validation_data=[x_valid, y_valid],
+                            epochs=epochs,
+                            batch_size=batch_size,
+                            callbacks=[early_stopping, model_checkpoint, reduce_lr],
+                            verbose=1)
+
+        # save training history
+        plt.plot(history.history['my_iou_metric'][1:])
+        plt.plot(history.history['val_my_iou_metric'][1:])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train','Validation'], loc='upper left')
+        plt.savefig('../output/model_loss'+str(n_fold)+'.png')
+
+        fig, (ax_loss, ax_acc) = plt.subplots(1, 2, figsize=(15,5))
+        ax_loss.plot(history.epoch, history.history["loss"], label="Train loss")
+        ax_loss.plot(history.epoch, history.history["val_loss"], label="Validation loss")
+        plt.savefig('../output/train_val_loss'+str(n_fold)+'.png')
+        plt.close()
+
+        # メモリ節約のための処理
+        del ids_train, ids_valid, x_train, y_train, x_valid, y_valid
+        del cov_train, cov_test, depth_train, depth_test
+        del input_layer, output_layer, model, early_stopping, model_checkpoint, reduce_lr
+        del history
+        gc.collect()
 
 def main():
-
-    # Set some parameters
-    img_size_ori = 101
-    img_size_target = 101
-    im_width = 101
-    im_height = 101
-    im_chan = 1
-    basicpath = '../input/'
-    path_train = basicpath + 'train/'
-    path_test = basicpath + 'test/'
-
-    path_train_images = path_train + 'images/'
-    path_train_masks = path_train + 'masks/'
-    path_test_images = path_test + 'images/'
 
     # Loading of training/testing ids and depths
     if os.path.isfile('../output/train_df.pkl') and os.path.isfile('../output/test_df.pkl'):
@@ -171,92 +214,8 @@ def main():
     else:
         train_df, test_df = get_input_data()
 
-    # Create train/validation split stratified by salt coverage
-    ids_train, ids_valid, x_train, x_valid, y_train, y_valid,\
-    cov_train, cov_test, depth_train, depth_test = train_test_split(train_df.index.values,
-                                                                    np.array(train_df.images.tolist()).reshape(-1, img_size_target, img_size_target, 1),
-                                                                    np.array(train_df.masks.tolist()).reshape(-1, img_size_target, img_size_target, 1),
-                                                                    train_df.coverage.values,
-                                                                    train_df.z.values,
-                                                                    test_size=0.2, stratify=train_df.coverage_class, random_state= 1234)
-
-    #Data augmentation
-    x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
-    y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
-    print("train shape: {}, test shape: {}".format(x_train.shape, y_train.shape))
-
-    # model
-    input_layer = Input((img_size_target, img_size_target, 1))
-    output_layer = build_model(input_layer, 16,0.5)
-
-    model = Model(input_layer, output_layer)
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=[my_iou_metric])
-
-    early_stopping = EarlyStopping(monitor='val_acc', mode = 'max',patience=20, verbose=1)
-    model_checkpoint = ModelCheckpoint("../output/unet_best1.model",monitor='val_acc',
-                                       mode = 'max', save_best_only=True, verbose=1)
-    reduce_lr = ReduceLROnPlateau(monitor='val_acc', mode = 'max',factor=0.2, patience=5, min_lr=0.00001, verbose=1)
-    #reduce_lr = ReduceLROnPlateau(factor=0.2, patience=5, min_lr=0.00001, verbose=1)
-
-    epochs = 200
-    batch_size = 32
-
-    history = model.fit(x_train, y_train,
-                        validation_data=[x_valid, y_valid],
-                        epochs=epochs,
-                        batch_size=batch_size,
-                        callbacks=[early_stopping, model_checkpoint, reduce_lr],
-                        verbose=1)
-
-    # save training history
-    plt.plot(history.history['acc'][1:])
-    plt.plot(history.history['val_acc'][1:])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train','Validation'], loc='upper left')
-    plt.savefig('model_loss.png')
-
-    fig, (ax_loss, ax_acc) = plt.subplots(1, 2, figsize=(15,5))
-    ax_loss.plot(history.epoch, history.history["loss"], label="Train loss")
-    ax_loss.plot(history.epoch, history.history["val_loss"], label="Validation loss")
-    plt.savefig('train_val_loss.png')
-
-    model = load_model("../output/unet_best1.model")
-    preds_valid = predict_result(model,x_valid,img_size_target)
-
-    ## Scoring for last model
-    thresholds = np.linspace(0.3, 0.7, 31)
-    ious = np.array([iou_metric(y_valid.reshape((-1, img_size_target, img_size_target)), [filter_image(img) for img in preds_valid > threshold]) for threshold in tqdm(thresholds)])
-
-    threshold_best_index = np.argmax(ious)
-    iou_best = ious[threshold_best_index]
-    threshold_best = thresholds[threshold_best_index]
-
-    plt.plot(thresholds, ious)
-    plt.plot(threshold_best, iou_best, "xr", label="Best threshold")
-    plt.xlabel("Threshold")
-    plt.ylabel("IoU")
-    plt.title("Threshold vs IoU ({}, {})".format(threshold_best, iou_best))
-    plt.legend()
-    plt.savefig('threshold.png')
-
-    del x_train, x_valid, y_train, y_valid, preds_valid
-    gc.collect()
-
-    x_test = np.array([(np.array(load_img("../input/test/images/{}.png".format(idx), color_mode = "grayscale"))) / 255 for idx in tqdm(test_df.index)]).reshape(-1, img_size_target, img_size_target, 1)
-    preds_test = predict_result(model,x_test,img_size_target)
-
-    t1 = time.time()
-    pred_dict = {idx: rle_encode(filter_image(preds_test[i] > threshold_best)) for i, idx in enumerate(tqdm(test_df.index.values))}
-    t2 = time.time()
-
-    print("Usedtime = "+ str(t2-t1)+" s")
-
-    sub = pd.DataFrame.from_dict(pred_dict,orient='index')
-    sub.index.names = ['id']
-    sub.columns = ['rle_mask']
-    sub.to_csv('../output/submission.csv')
+    # training
+    kfold_training(train_df, test_df, NUM_FOLDS, stratified = True, debug= False)
 
 if __name__ == '__main__':
     main()
