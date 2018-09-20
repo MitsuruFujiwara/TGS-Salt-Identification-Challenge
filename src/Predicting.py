447b2ab45c7b003abbe7c04376fd3395751355e3
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-09-20 21:11:17 +0900

    LB#15

--- src/Predicting.py
+++ src/Predicting.py
@@ -10,8 +10,8 @@ from tqdm import tqdm
 from keras.models import load_model
 from keras.preprocessing.image import load_img
 
-from Utils import predict_result, loadpkl, my_iou_metric_2, rle_encode, filter_image, iou_metric, line_notify, upsample
-from Utils import IMG_SIZE_TARGET, NUM_FOLDS
+from Utils import predict_result, loadpkl, my_iou_metric_2, rle_encode, filter_image, iou_metric, line_notify, upsample, downsample
+from Utils import IMG_SIZE_TARGET, IMG_SIZE_ORI, NUM_FOLDS
 from lovasz_losses_tf import keras_lovasz_softmax
 
 """
@@ -29,11 +29,11 @@ def main():
     x_test = np.array([(upsample(np.array(load_img("../input/test/images/{}.png".format(idx), color_mode = "grayscale")))) / 255 for idx in tqdm(test_df.index)]).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
     y_train = np.array(train_df.masks.map(upsample).tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
 
+    # 結果保存用の配列
+    sub_preds = np.zeros((x_test.shape[0], IMG_SIZE_ORI, IMG_SIZE_ORI))
+
     # (128, 128, 3)に変換
     x_test = np.repeat(x_test,3,axis=3)
-
-    # 結果保存用の配列
-    sub_preds = np.zeros((x_test.shape[0], x_test.shape[1], x_test.shape[2]))
 
     print('Generating submission file...')
 
@@ -47,7 +47,7 @@ def main():
                                            'keras_lovasz_softmax':keras_lovasz_softmax})
 
         # testデータの予測値を保存
-        sub_preds_single = predict_result(model, x_test ,IMG_SIZE_TARGET)
+        sub_preds_single = np.array([downsample(x) for x in tqdm(predict_result(model, x_test ,IMG_SIZE_TARGET))])
         sub_preds += sub_preds_single / NUM_FOLDS
 
         # single modelのsubmission fileを保存（threshold=0）
