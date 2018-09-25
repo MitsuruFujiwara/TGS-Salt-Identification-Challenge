401ac804689ce277fc10b20b7a95b9aa0be6d562
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-09-25 21:00:21 +0900

    PretrainedじゃないUnetResNet34を使う方針に変更

--- src/Learning.py
+++ src/Learning.py
@@ -34,6 +34,61 @@ Preprocessingで作成したファイルを読み込み、モデルを学習す�
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+###################################################################
+# Define New Loss Function
+###################################################################
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+    loss = weight * (logit_y_pred * (1. - y_true) +
+                     K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+def weighted_dice_loss(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    loss = 1. - K.sum(score)
+    return loss
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd
+    averaged_mask = K.pool2d(
+            y_true, pool_size=(50, 50), strides=(1, 1), padding='same', pool_mode='avg')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight = 5. * K.exp(-5. * K.abs(averaged_mask - 0.5))
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + dice_loss(y_true, y_pred)
+    return loss
+
 def BatchActivate(x):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
@@ -148,8 +203,8 @@ def kfold_training(train_df, num_folds, stratified = True, debug= False):
     else:
         folds = KFold(n_splits= num_folds, shuffle=True, random_state=47)
 
-    X = np.array(train_df.images.tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
-    Y = np.array(train_df.masks.tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
+    X = np.array(train_df.images.map(upsample).tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
+    Y = np.array(train_df.masks.map(upsample).tolist()).reshape(-1, IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1)
     cov = train_df.coverage.values
     depth = train_df.z.values
 
@@ -192,76 +247,36 @@ def kfold_training(train_df, num_folds, stratified = True, debug= False):
         print("train shape: {}, test shape: {}".format(x_train.shape, x_valid.shape))
 
         # model
-        input_layer = Input((IMG_SIZE_TARGET, IMG_SIZE_TARGET, 1))
-        output_layer = build_model(input_layer, 16, 0.5)
-        model_bin = Model(input_layer, output_layer)
+        model = UResNet34(input_shape=(1,IMG_SIZE_TARGET,IMG_SIZE_TARGET))
 
-        # 最初にlossをbinary_crossentropyにしたモデルを推定します
-        if not(os.path.isfile('../output/unet_best_bin'+str(n_fold)+'.model')):
-            model_bin.compile(loss="binary_crossentropy", optimizer=adam(lr = 0.01), metrics=[my_iou_metric])
+        # compile
+        model.compile(loss=bce_dice_loss, optimizer="adam", metrics=[my_iou_metric])
 
-            early_stopping_bin = EarlyStopping(monitor='my_iou_metric',
-                                               mode = 'max',
-                                               patience=10,
-                                               verbose=1)
-
-            model_checkpoint_bin = ModelCheckpoint('../output/unet_best_bin'+str(n_fold)+'.model',
-                                                   monitor='val_my_iou_metric',
-                                                   mode = 'max',
-                                                   save_best_only=True,
-                                                   verbose=1)
-
-            reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric',
-                                          mode = 'max',
-                                          factor=0.5,
-                                          patience=5,
-                                          min_lr=0.0001,
-                                          verbose=1)
-
-            history_bin = model_bin.fit(x_train, y_train,
-                                        validation_data=[x_valid, y_valid],
-                                        epochs=200,
-                                        batch_size=32,
-                                        callbacks=[early_stopping_bin, model_checkpoint_bin, reduce_lr],
-                                        verbose=1)
-
-            del model_bin, early_stopping_bin, model_checkpoint_bin, history_bin
-            gc.collect()
-
-        # binary_crossentropyで推定したモデルをロード
-        model = load_model('../output/unet_best_bin'+str(n_fold)+'.model',
-                           custom_objects={'my_iou_metric': my_iou_metric})
-
-        # remove layter activation layer and use losvasz loss
-        input_x = model.layers[0].input
-        output_layer = model.layers[-1].input
-        model = Model(input_x, output_layer)
-
-        # lossをLovasz Lossにしたモデルを推定
-        model.compile(loss=keras_lovasz_softmax, optimizer=adam(lr = 0.01), metrics=[my_iou_metric_2])
-
-        early_stopping = EarlyStopping(monitor='val_my_iou_metric_2',
+        early_stopping = EarlyStopping(monitor='val_my_iou_metric',
                                        mode='max',
                                        patience=20,
                                        verbose=1)
 
-        model_checkpoint = ModelCheckpoint('../output/unet_best'+str(n_fold)+'.model',
-                                           monitor='val_my_iou_metric_2',
+        model_checkpoint = ModelCheckpoint('../output/UnetResNet34_'+str(n_fold)+'.model',
+                                           monitor='val_my_iou_metric',
                                            mode = 'max',
                                            save_best_only=True,
                                            verbose=1)
 
-        reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric_2',
+        reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric',
                                       mode = 'max',
                                       factor=0.2,
                                       patience=5,
                                       min_lr=0.0001,
                                       verbose=1)
 
+        epochs = 30
+        batch_size = 32
+
         history = model.fit(x_train, y_train,
                             validation_data=[x_valid, y_valid],
-                            epochs=200,
-                            batch_size=32,
+                            epochs=epochs,
+                            batch_size=batch_size,
                             callbacks=[early_stopping, model_checkpoint, reduce_lr],
                             verbose=1)
 
